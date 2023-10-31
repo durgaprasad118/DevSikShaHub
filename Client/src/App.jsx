9cb7dafbd7fd8d8de4fc573a92c392152cf1aef5
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2023-11-01 01:27:49 +0530

    delete all courses slice unwanted file

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,10 @@
-import { useGetCoursesQuery } from './redux/slices/courseApiSlice'
+
 
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Navbar/Navbar'
 function App() {
-  // const { isLoading, isError, isSuccess, data, error } =
-  //   useGetCoursesQuery('available')
-  // console.log(isLoading, isError, isSuccess, data)
-
-  return (
+   return (
     <>
       <Header></Header>
 
--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,22 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { setupListeners } from '@reduxjs/toolkit/query'
-import { coursesApi } from './slices/courseApiSlice'
-import { userApiSlice } from './api/userApi'
-import authReducer from './slices/authSlice'
-import { adminCourseSlice } from './api/adminApi'
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { userApiSlice } from "./api/userApi";
+import authReducer from "./slices/authSlice";
+import { adminCourseSlice } from "./api/adminApi";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    [coursesApi.reducerPath]: coursesApi.reducer,
     [userApiSlice.reducerPath]: userApiSlice.reducer,
     [adminCourseSlice.reducerPath]: adminCourseSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
-      coursesApi.middleware,
       userApiSlice.middleware,
-      adminCourseSlice.middleware,
+      adminCourseSlice.middleware
     ),
-})
+});
 
-setupListeners(store.dispatch)
+setupListeners(store.dispatch);
