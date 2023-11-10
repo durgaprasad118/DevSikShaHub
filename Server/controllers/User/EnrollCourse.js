7ec559984964b27a8d19ee545f1502251fcd4240
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2023-11-11 04:06:43 +0530

    update in getenrolledCourses

--- Server/controllers/User/EnrollCourse.js
+++ Server/controllers/User/EnrollCourse.js
@@ -1,27 +1,23 @@
-import User from "../../Model/User.js";
-import Course from "../../Model/Courses.js";
-const EnrollCourse = async(req,res)=>{
-    try {
-        const {courseId}= req.body;
-        const userId = req.User._id;
-        const user = await User.findById(userId);
-        const course = await Course.findById(courseId);
-        if(!course){
-            return res.status(400).json({ message: 'Course not found' }); 
-        }
-        if(user.Enrolledcourses.includes(courseId)){
-            return res.status(400).json({ message: 'User is already enrolled in the course' });
-        }
-        user.Enrolledcourses.push(courseId);
-        await user.save();
-        res.status(200).json({
-            message:'Course Added Successfully'
-        })
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
+import User from '../../Model/User.js'
+import Course from '../../Model/Courses.js'
+const EnrollCourse = async (req, res) => {
+  try {
+    const userId = req.User._id
+    const user = await User.findById(userId)
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' })
     }
-    
+    const CartItems = user.Store
+    CartItems.forEach((id) => {
+      if (!user.Enrolledcourses.includes(id)) {
+        user.Enrolledcourses.push(id)
+      }
+    })
+    await user.save()
+    res.json({ enrolledCourses: user.Enrolledcourses, message: 'Courses enrolled successfully' })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
 }
-export default EnrollCourse;
+export default EnrollCourse
--- Server/controllers/User/getEnrolledCourses.js
+++ Server/controllers/User/getEnrolledCourses.js
@@ -1,21 +1,19 @@
-import User from "../../Model/User.js";
-import Course from "../../Model/Courses.js";
+import User from '../../Model/User.js'
+import Course from '../../Model/Courses.js'
 
-const enrolledCourses = async(req,res)=>{
-    try {
-        const userId = req.User._id;
-        const user = await User.findById(userId);
-        if (!user) {
-          return res.status(404).json({ message: 'User not found' });
-        }
-        const enrolled = user.Enrolledcourses;
-        console.log(enrolled);
-        const courses = await Course.find({ _id: { $in:enrolled } });
-        res.status(200).json({ courses });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-      }
-    
+const enrolledCourses = async (req, res) => {
+  try {
+    const userId = req.User._id
+    const user = await User.findById(userId)
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+    const enrolled = user.Enrolledcourses
+    const courses = await Course.find({ _id: { $in: enrolled } })
+    res.status(200).json({ courses })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
 }
-export default enrolledCourses;
+export default enrolledCourses
