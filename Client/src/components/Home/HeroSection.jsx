e6afda6de3e2d4a930b343b79854ce777ac5924e
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2023-11-15 17:12:58 +0530

    hero section update

--- Client/src/components/Home/HeroSection.jsx
+++ Client/src/components/Home/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
         </svg>
       </a>
       <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Upskill & Grab your <span className='text-blue-600'> Dream Offer</span></h1>
-      <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+      <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">DevSikShaHub, the go to place for affordable and quality content</p>
       <form className="w-full max-w-md mx-auto">   
         <label htmlFor="default-email" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Email sign-up</label>
         <div className="relative">
