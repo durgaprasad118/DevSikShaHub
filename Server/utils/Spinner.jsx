d60274ed05865b692f05d102a7a32524a33c456a
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2023-11-25 04:01:39 +0530

    deleted unnecessary file

--- Server/utils/Spinner.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { ImSpinner } from 'react-icons/im'
-const Spinner = ({color}) => {
-  return <ImSpinner className="animate-spin text-2xl text-white" />
-}
-
-export default Spinner
